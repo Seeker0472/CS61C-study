f30f12e593332c573aec1f97ad471099fc396305
seeker0472 <gmx472@qq.com>
2024-05-02 16:56:38 +0800

    完成了PAT2

--- su22-proj2-starter/src/relu.s
+++ su22-proj2-starter/src/relu.s
@@ -14,23 +14,31 @@
 # ==============================================================================
 relu:
 	# Prologue
+ebreak
 
+addi a2,zero,0 # a2=0
+
+# 如果小于1就直接退出
+bge a1,zero,loop_start
+addi a0,zero,36
+j exit
 
 loop_start:
 
-
-
-
-
-
+lw a3,0(a0)
+bge a3,zero,loop_continue
+sw zero,0(a0)
 
 
 loop_continue:
 
+addi a2,a2,1
+addi a0,a0,4
+blt a2,a1,loop_start
 
 
 loop_end:
-
+addi a0,zero,0
 
 	# Epilogue
 
